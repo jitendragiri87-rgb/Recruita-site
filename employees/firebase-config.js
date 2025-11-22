d8c0cc83873137a08e74c4091dc331c605423093
jitendragiri87-rgb <jitendragiri87@gmail.com>
2025-11-23 02:39:03 +0530

    Create firebase-config.js

---
+++ employees/firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCQi68RIeS4L5SwNQuAYFbK7KB3awrADV0",
+  authDomain: "authentication-114b0.firebaseapp.com",
+  projectId: "authentication-114b0",
+  storageBucket: "authentication-114b0.appspot.com",
+  messagingSenderId: "332309567366",
+  appId: "1:332309567366:web:0312c801fe2193e3debe07"
+};
