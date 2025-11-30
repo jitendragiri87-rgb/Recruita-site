9cdef956f26d50ff74b2a0fa03110d16705250c6
jitendragiri87-rgb <jitendragiri87@gmail.com>
2025-11-30 23:31:23 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,9 +1,7 @@
-<script type="module">
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
 
-// Your Firebase Config
 export const firebaseConfig = {
   apiKey: "AIzaSyCQi68RIeS4L5SwNQuAYFbK7KB3awrADV0",
   authDomain: "authentication-114b0.firebaseapp.com",
@@ -14,8 +12,6 @@ export const firebaseConfig = {
   measurementId: "G-F9PQ3DE71D"
 };
 
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-</script>
